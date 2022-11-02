4a6f2bb98d75667a98e4e70b022c97c42deec0e6
juliocesar.chaconvilela.contractor.gdevtools.com <juliocesar.chaconvilela.contractor@gdevtools.com>
2022-11-02 17:34:54 -0500

    first commit

---
+++ bkp.py
@@ -0,0 +1,29 @@
+import pywhatkit
+from time import sleep
+
+#pywhatkit.sendwhatmsg("pywhatkit.sendwhats_image("+51981236649","Imagen", 23,27)
+#Pruebas: 51981236649, 51941456697, 51965710823, 51989074542
+pywhatkit.sendwhats_image("+51954435776","D:\proyectos-python\sendmessage\invitacion.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar de los talleres _Alivio en el dolor y Bálsamo de esperanza_ dirigido a aquellas personas y familiares que han perdido un ser querido. Estos talleres se realizarán:\n\n✅ Días: viernes 21 y 28 de enero\n✅ Hora: 7:30pm\n✅ Lugar:  Av. Arequipa 2356 (templo)\n✅ Tallerista: Hna Elizabeth Takuri\n✅ *Puede invitar a más familiares*\n✅ Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n✅ *Se transmitirá por Facebook y Youtube*\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(5)
+
+pywhatkit.sendwhats_image("","D:\proyectos-python\sendmessage\invitacion.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar de los talleres _Alivio en el dolor y Bálsamo de esperanza_ dirigido a aquellas personas y familiares que han perdido un ser querido. Estos talleres se realizarán:\n\n✅ Días: viernes 21 y 28 de enero\n✅ Hora: 7:30pm\n✅ Lugar:  Av. Arequipa 2356 (templo)\n✅ Tallerista: Hna Elizabeth Takuri\n✅ *Puede invitar a más familiares*\n✅ Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n✅ *Se transmitirá por Facebook y Youtube*\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(5)
+
+pywhatkit.sendwhats_image("","D:\proyectos-python\sendmessage\invitacion.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar de los talleres _Alivio en el dolor y Bálsamo de esperanza_ dirigido a aquellas personas y familiares que han perdido un ser querido. Estos talleres se realizarán:\n\n✅ Días: viernes 21 y 28 de enero\n✅ Hora: 7:30pm\n✅ Lugar:  Av. Arequipa 2356 (templo)\n✅ Tallerista: Hna Elizabeth Takuri\n✅ *Puede invitar a más familiares*\n✅ Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n✅ *Se transmitirá por Facebook y Youtube*\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(5)
+
+pywhatkit.sendwhats_image("","D:\proyectos-python\sendmessage\invitacion.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar de los talleres _Alivio en el dolor y Bálsamo de esperanza_ dirigido a aquellas personas y familiares que han perdido un ser querido. Estos talleres se realizarán:\n\n✅ Días: viernes 21 y 28 de enero\n✅ Hora: 7:30pm\n✅ Lugar:  Av. Arequipa 2356 (templo)\n✅ Tallerista: Hna Elizabeth Takuri\n✅ *Puede invitar a más familiares*\n✅ Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n✅ *Se transmitirá por Facebook y Youtube*\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(5)
+
+pywhatkit.sendwhats_image("","D:\proyectos-python\sendmessage\invitacion.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar de los talleres _Alivio en el dolor y Bálsamo de esperanza_ dirigido a aquellas personas y familiares que han perdido un ser querido. Estos talleres se realizarán:\n\n✅ Días: viernes 21 y 28 de enero\n✅ Hora: 7:30pm\n✅ Lugar:  Av. Arequipa 2356 (templo)\n✅ Tallerista: Hna Elizabeth Takuri\n✅ *Puede invitar a más familiares*\n✅ Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n✅ *Se transmitirá por Facebook y Youtube*\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(5)
+
+
+pywhatkit.sendwhats_image("+51977274336","D:\proyectos-python\sendmessage\invitacion2.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar del taller _Bálsamo de esperanza_ dirigido a aquellas personas y familiares que pasan por la prueba de haber perdido un ser querido. Este taller se realizará:\n\n- Día: viernes 28 de enero\n- Hora: 7:30pm\n- Lugar:  Av. Arequipa 2356 (templo)\n✅ Tallerista: Hna Elizabeth Takuri\n- Puede invitar a más familiares*\n- Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n- Se transmitirá por Facebook y Youtube*\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(5)
---
+++ main.py
@@ -0,0 +1,15 @@
+import pywhatkit
+from time import sleep
+
+pywhatkit.sendwhats_image("+51954715721","D:\proyectos-python\sendmessage\invitacion2.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar del taller _Bálsamo de esperanza_ dirigido a aquellas personas y familiares que pasan por la prueba de haber perdido un ser querido. Este taller se realizará:\n\n- Día: viernes 28 de enero\n- Hora: 7:30pm\n- Lugar: Av. Arequipa 2356 (templo)\n- Tallerista: Hna Elizabeth Takuri\n- Puede invitar a más familiares\n- Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n- Se transmitirá por Facebook y Youtube\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(4)
+
+pywhatkit.sendwhats_image("+51935971469","D:\proyectos-python\sendmessage\invitacion2.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar del taller _Bálsamo de esperanza_ dirigido a aquellas personas y familiares que pasan por la prueba de haber perdido un ser querido. Este taller se realizará:\n\n- Día: viernes 28 de enero\n- Hora: 7:30pm\n- Lugar: Av. Arequipa 2356 (templo)\n- Tallerista: Hna Elizabeth Takuri\n- Puede invitar a más familiares\n- Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n- Se transmitirá por Facebook y Youtube\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(4)
+
+pywhatkit.sendwhats_image("+51981236649","D:\proyectos-python\sendmessage\invitacion2.jpeg", 
+"Reciba un saludo cordial de nuestra Iglesia Alianza Cristiana y Misionera de Lince.\nLe invitamos a participar del taller _Bálsamo de esperanza_ dirigido a aquellas personas y familiares que pasan por la prueba de haber perdido un ser querido. Este taller se realizará:\n\n- Día: viernes 28 de enero\n- Hora: 7:30pm\n- Lugar: Av. Arequipa 2356 (templo)\n- Tallerista: Hna Elizabeth Takuri\n- Puede invitar a más familiares\n- Se está cumpliendo con todos los protocolos respectivos. No olvidar llevar su carnet de vacunación.\n- Se transmitirá por Facebook y Youtube\n\n¡Dios le bendiga!\n\n_El Señor te protegerá; de todo mal protegerá tu vida. El Señor te cuidará en el hogar y en el camino, desde ahora y para siempre_.\nSalmo 121:7-8\n\nCualquier consulta comunicarse con el 989074542 (Caleb Sovero)")
+sleep(4)
+
